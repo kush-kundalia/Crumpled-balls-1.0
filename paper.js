8ffb90d17978f883a22a23b4a920dc3659fc9c32
kush-kundalia <72961844+kush-kundalia@users.noreply.github.com>
2021-01-19 19:36:57 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,26 @@
+class Paper{
+    constructor(x, y ,r){
+        const options = {
+            restitution: 0.3,
+            friction :0.5 ,
+            density :1.2 ,
+            isStatic : false
+          };
+    this.body = Matter.Bodies.circle(x,y,r,options);
+    Matter.World.add(world , this.body);
+    // this.r = r;
+    // restitution = 2; 
+}
+
+display(){
+    const pos = this.body.position;
+    const angle = this.body.angle;
+    push();
+    translate(pos.x , pos.y);
+    rotate(angle);
+    fill(255,20,147);
+    strokeWeight(0.5);
+    circle(0, 0, this.r);
+    pop();
+}
+}
